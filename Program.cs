f7a212d7a7fcf0f040598cdc03a28754c7b99b94
mazylol <landonp95@gmail.com>
2022-11-16 23:22:34 -0500

    stuff

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Balls");
+        }
+    }
+}
